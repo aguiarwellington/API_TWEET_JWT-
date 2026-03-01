bd50a9ae7f1584ed48690b35f0a3f0fcee18c802
Wellington Carvalho <88244614+aguiarwellington@users.noreply.github.com>
2026-03-01 14:57:50 -0300

    Criacao das roles e docker

---
+++ demo/src/main/java/Auten/demo/repository/TweetRepository.java
@@ -0,0 +1,12 @@
+package Auten.demo.repository;
+
+import Auten.demo.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
---
+++ demo/src/main/java/Auten/demo/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package Auten.demo.repository;
+
+public interface UserRepository {
+}
